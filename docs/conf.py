9da6d2ce8b7875322701a9f308527a8e23023342
Yves Mercadier <5513375+ym001@users.noreply.github.com>
2024-08-16 11:45:39 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,3 +26,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# Add custom CSS files
+html_css_files = [
+    'custom.css',
+]
