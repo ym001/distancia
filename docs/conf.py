2b23e58a2fd909b23212d10d7ef021141f118d6e
Yves Mercadier <5513375+ym001@users.noreply.github.com>
2024-08-16 11:51:20 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'solar'
 html_static_path = ['_static']
 
 # Add custom CSS files
