a605282f686232b5902a0601e3f9c85d8e720a78
Yves Mercadier <5513375+ym001@users.noreply.github.com>
2024-08-21 11:14:33 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,10 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [       
-  'sphinx_copybutton'
+  'sphinx_copybutton','sphinxcontrib.bibtex'
 ]
+
+bibtex_bibfiles = ['refs.bib']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
