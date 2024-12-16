c5de9ef4ba56cd1d7b15a789418f4b8650e0e3bd
Yves Mercadier <5513375+ym001@users.noreply.github.com>
2024-12-16 10:31:11 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,8 @@ release = '0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [       
-  'sphinx_copybutton','sphinxcontrib.bibtex','sphinx_sitemap',"nbsphinx"
+  'sphinx_copybutton','sphinxcontrib.bibtex','sphinx_sitemap',"nbsphinx",'sphinxcontrib.enumitem',
+
 ]
 
 bibtex_bibfiles = ['refs.bib']
